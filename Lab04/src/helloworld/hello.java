2ed87e30cb8d595544245981fdf7480c138ed6c2
spencehood <spho6674@colorado.edu>
2014-09-17 14:07:31 -0600

    Update hello.java
    Added another line for proof

--- Lab04/src/helloworld/hello.java
+++ Lab04/src/helloworld/hello.java
@@ -5,5 +5,6 @@ public class hello {
 		System.out.println("Hello World!");
 		System.out.println("How are you?");
 		System.out.println("Adding this in Github");
+		System.out.println("Also added in GH");
 	}
 }
