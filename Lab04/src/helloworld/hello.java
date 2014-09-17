2bb3e921580b7327f24fc58c723933aef24d853c
spencehood <spho6674@colorado.edu>
2014-09-17 13:57:20 -0600

    Update hello.java
    Added another message

--- Lab04/src/helloworld/hello.java
+++ Lab04/src/helloworld/hello.java
@@ -4,5 +4,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("How are you?");
+		System.out.println("Adding this in Github");
 	}
 }
